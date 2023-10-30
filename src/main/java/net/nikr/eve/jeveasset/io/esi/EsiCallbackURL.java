f280bd4393e051619314d42a322355af6a7ce63c
Rihcard <hualuoyu_yks@qq.com>
2023-10-30 08:31:20 +0800

    更新国服ESI地址

--- src/main/java/net/nikr/eve/jeveasset/io/esi/EsiCallbackURL.java
+++ src/main/java/net/nikr/eve/jeveasset/io/esi/EsiCallbackURL.java
@@ -23,7 +23,7 @@ package net.nikr.eve.jeveasset.io.esi;
 
 public enum EsiCallbackURL {
 	LOCALHOST("http://localhost:2221", ""),
-	EVE_163_CODE("https://esi.evepc.163.com/ui/oauth2-redirect.html", "bc90aa496a404724a93f41b4f4e97761"),
+	EVE_163_CODE("https://ali-esi.evepc.163.com/ui/oauth2-redirect.html", "bc90aa496a404724a93f41b4f4e97761"),
 
 	;
 	private final String url;
